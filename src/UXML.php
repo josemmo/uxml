fb34a2a2f84ecdb30841c66e58bb3c02ccc62e88
José Miguel Moreno <josemmo@pm.me>
2022-02-19 19:44:54 +0100

    Improved linting
    - Updated UXML

--- src/UXML.php
+++ src/UXML.php
@@ -11,12 +11,13 @@ use function preg_replace_callback;
 use function strpos;
 
 class UXML {
-    const NS_PREFIX = "__uxml_ns_";
+    const NS_PREFIX = '__uxml_ns_';
     /** @var DOMElement */
     protected $element;
 
     /**
      * Create instance from XML string
+     * 
      * @param  string $xmlString XML string
      * @return self              Root XML element
      * @throws InvalidArgumentException if failed to parse XML
@@ -30,9 +31,9 @@ class UXML {
         return new self($doc->documentElement);
     }
 
-
     /**
      * Create instance from DOM element
+     * 
      * @param  DOMElement $element DOM element
      * @return self                Wrapped element as a UXML instance
      * @suppress PhanUndeclaredProperty
@@ -41,9 +42,9 @@ class UXML {
         return $element->uxml ?? new self($element);
     }
 
-
     /**
      * Create new instance
+     * 
      * @param  string               $name  Element tag name
      * @param  string|null          $value Element value or `null` for empty
      * @param  array<string,string> $attrs Element attributes
@@ -65,7 +66,7 @@ class UXML {
 
         // Set attributes
         foreach ($attrs as $attrName=>$attrValue) {
-            if ($attrName === "xmlns" || strpos($attrName, 'xmlns:') === 0) {
+            if ($attrName === 'xmlns' || strpos($attrName, 'xmlns:') === 0) {
                 $domElement->setAttributeNS('http://www.w3.org/2000/xmlns/', $attrName, $attrValue);
             } else {
                 $domElement->setAttribute($attrName, $attrValue);
@@ -76,9 +77,9 @@ class UXML {
         return new self($domElement);
     }
 
-
     /**
      * Class constructor
+     * 
      * @param DOMElement $element DOM Element instance
      * @suppress PhanUndeclaredProperty
      */
@@ -87,18 +88,18 @@ class UXML {
         $this->element->uxml = $this;
     }
 
-
     /**
      * Get DOM element instance
+     * 
      * @return DOMElement DOM element instance
      */
     public function element(): DOMElement {
         return $this->element;
     }
 
-
     /**
      * Get parent element
+     * 
      * @return self Parent element instance or this instance if it has no parent
      */
     public function parent(): self {
@@ -106,20 +107,20 @@ class UXML {
         return ($parentNode !== null && $parentNode instanceof DOMElement) ? self::fromElement($parentNode) : $this;
     }
 
-
     /**
      * Is empty
-     * @return boolean TRUE if the element has no inner content, FALSE otherwise
+     * 
+     * @return boolean `true` if the element has no inner content, `false` otherwise
      */
     public function isEmpty(): bool {
         return ($this->element->childNodes->length === 0);
     }
 
-
     /**
      * Add child element
+     * 
      * @param  string      $name  New element tag name
-     * @param  string|null $value New element value or NULL for empty
+     * @param  string|null $value New element value or `null` for empty
      * @param  array       $attrs New element attributes
      * @return self               New element instance
      * @throws DOMException if failed to create child element
@@ -130,9 +131,9 @@ class UXML {
         return $child;
     }
 
-
     /**
      * Find elements
+     * 
      * @param  string   $xpath XPath query relative to this element
      * @param  int|null $limit Maximum number of results to return
      * @return self[]          Matched elements
@@ -159,15 +160,15 @@ class UXML {
         foreach ($domNodes as $domNode) {
             if (!$domNode instanceof DOMElement) continue;
             $res[] = self::fromElement($domNode);
-            if (($limit !== null) && (--$limit <= 0)) break;
+            if ($limit !== null && --$limit <= 0) break;
         }
 
         return $res;
     }
 
-
     /**
      * Find one element
+     * 
      * @param  string    $xpath XPath query relative to this element
      * @return self|null        First matched element or NULL if not found
      */
@@ -175,7 +176,6 @@ class UXML {
         $res = $this->getAll($xpath, 1);
         return $res[0] ?? null;
     }
-
 
     /**
      * Remove this element
@@ -190,24 +190,24 @@ class UXML {
         }
     }
 
-
     /**
      * Export element and children as text
+     * 
      * @return string Text representation
      */
     public function asText(): string {
         return $this->element->textContent;
     }
 
-
     /**
      * Export as XML string
-     * @param  string|null $version  Document version, NULL for no declaration
+     * 
+     * @param  string|null $version  Document version, `null` for no declaration
      * @param  string      $encoding Document encoding
      * @param  boolean     $format   Format output
      * @return string                XML string
      */
-    public function asXML(?string $version="1.0", string $encoding="UTF-8", bool $format=true): string {
+    public function asXML(?string $version='1.0', string $encoding='UTF-8', bool $format=true): string {
         $doc = new DOMDocument();
 
         // Define document properties
@@ -229,7 +229,6 @@ class UXML {
 
         return $res;
     }
-
 
     /**
      * @inheritdoc
